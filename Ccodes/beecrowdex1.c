f13e022a6f3b9970c8b91a1e7db75a40ffc99aeb
Guilherme Teles <guilherme.teles.engineer@gmail.com>
2024-02-17 20:29:07 -0400

    On branch main  Changes to be committed: 	new file:   Ccodes/.vscode/c_cpp_properties.json 	new file:   Ccodes/.vscode/launch.json 	new file:   Ccodes/.vscode/settings.json 	new file:   Ccodes/.vscode/tasks.json 	new file:   Ccodes/beecrowdex1 	new file:   Ccodes/beecrowdex1.c 	new file:   Ccodes/beecrowdex2.c 	new file:   Ccodes/helloworld.c 	new file:   Ccodes/printainteger 	new file:   Ccodes/printainteger.c 	new file:   HTMLcodes/.vscode/c_cpp_properties.json 	new file:   HTMLcodes/.vscode/launch.json 	new file:   HTMLcodes/.vscode/settings.json 	new file:   HTMLcodes/.vscode/tasks.json 	new file:   HTMLcodes/CSS/style.css 	new file:   HTMLcodes/arrays.html 	new file:   HTMLcodes/condicoes.html 	new file:   HTMLcodes/eleitores.html 	new file:   HTMLcodes/for.html 	new file:   HTMLcodes/introducao.html 	new file:   HTMLcodes/javascript.html 	new file:   HTMLcodes/mysite.html 	new file:   HTMLcodes/switch.html 	new file:   HTMLcodes/testeprova.html 	new file:   HTMLcodes/testes1.html 	new file:   HTMLcodes/while.html

---
+++ Ccodes/beecrowdex1.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //Here we are import the standard lib to valid our code base functions.
+int main(){ //Here we are inform to our program the main code.
+
+    printf("Hello World!\n"); //Here we are using the command from standard lib to show something in the screen.
+
+    return 0; //Here we are return to the code output the number 0, 0 is our way to say the code sucess.
+}
---
+++ Ccodes/printainteger.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int number;
+    printf("Write a number");
+    scanf("%d", &number);
+    printf("The wrote number was %d.", number);
+    return 0;
+}
